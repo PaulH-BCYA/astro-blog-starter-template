f74f605bd47277bb0258049abaf0815616fd67ea
PaulH-BCYA <mrpaulhartsock@gmail.com>
2025-05-20 13:58:38 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Baldwin County Young Adults";
+export const SITE_DESCRIPTION = "Follow us on socials to stay up to date!";
